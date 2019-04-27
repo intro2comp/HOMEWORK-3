67f62d4fc93a1bca4756464c8a9c1e19b06ca085
Ted Moseley <tmoseley1106@gmail.com>
2019-04-27 18:03:34 -0400

    Add addition prints and mutex locks

--- problem_7/main.c
+++ problem_7/main.c
@@ -3,25 +3,39 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 
+#include "queue.h"
+
 #define TAXI_COUNT 5
-#define CUSTOMER_COUNT 2
+#define CUSTOMER_COUNT 10
 
 bool QUIT = false;
 
 pthread_t *customers, *taxis;  // Thread arrays
 sem_t sem_customer, sem_taxi;  // Counting semaphores
 
+pthread_mutex_t lock_taxis;
+pthread_cond_t taxis_available, taxis_used;
+
+Queue *customer_queue, *taxi_queue;
+
 void *taxi(void *arg)
 {
-    int index = *((int *) arg);
+    int *index = (int *) arg;
 
     while (!QUIT) {
-        sem_post(&sem_taxi);
-        printf("Taxi %d ready\n", index);
+        pthread_mutex_lock(&lock_taxis);  // Gain access to the taxi queue
+        while (queue_is_full(taxi_queue)) {
+            printf("Queue is full!\n");
+            pthread_cond_wait(&taxis_used, &lock_taxis);
+        }
+        queue_push(taxi_queue, *index);    // Push taxi index onto queue
+        printf("Taxi %d (%p) added to the queue\n", *index, arg);
+        pthread_mutex_unlock(&lock_taxis);
+        pthread_cond_signal(&taxis_available);
         sem_wait(&sem_customer);
-        printf("Customer found\n");
         // Taxi picks up customer...
         // done();
     }
@@ -38,7 +52,15 @@ void *customer(void *arg)
     while (!QUIT) {
         sem_post(&sem_customer);
         printf("Customer %d ready\n", index);
-        sem_wait(&sem_taxi);
+        pthread_mutex_lock(&lock_taxis);
+        while (queue_is_empty(taxi_queue)) {
+            printf("Queue is empty!\n");
+            pthread_cond_wait(&taxis_available, &lock_taxis);
+        }
+        printf("Customer %d boarding taxi %d\n", index, queue_pop(taxi_queue));
+        pthread_mutex_unlock(&lock_taxis);
+        pthread_cond_signal(&taxis_used);
+        sleep(rand() % 10);
         // Customer boards taxi...
         // done();
     }
@@ -50,17 +72,36 @@ void *customer(void *arg)
 
 int main()
 {
+    srand(time(NULL));
+
     // Initialize counting semaphores
     sem_init(&sem_customer, 0, 0);
     sem_init(&sem_taxi, 0, 0);
 
+    // Initialize locking semaphores
+    pthread_mutex_init(&lock_taxis, NULL);
+
+    pthread_cond_init(&taxis_available, NULL);
+    pthread_cond_init(&taxis_used, NULL);
+
     customers = malloc(CUSTOMER_COUNT * sizeof(pthread_t));
     taxis = malloc(TAXI_COUNT * sizeof(pthread_t));
 
+    int *ids = malloc(TAXI_COUNT * sizeof(int));
+
+    for (int j = 0; j < TAXI_COUNT; j++) {
+        ids[j] = j;
+    }
+
+    for (int j = 0; j < TAXI_COUNT; j++) {
+        printf("Index: %d\n", ids[j]);
+    }
+
+    taxi_queue = queue_init(TAXI_COUNT);
+
     for (int i = 0; i < TAXI_COUNT; i++) {
-        int *arg = malloc(sizeof(int));
-        *arg = i;
-        pthread_create(&(taxis[i]), NULL, taxi, arg);
+        printf("Creating taxi %d with arg %p\n", i, (void *) &ids[i]);
+        pthread_create(&(taxis[i]), NULL, taxi, (void *) &ids[i]);
     }
 
     printf("Taxis created\n");
@@ -78,6 +119,10 @@ int main()
     for (int i = 0; i < CUSTOMER_COUNT; i++) {
         pthread_join(customers[i], NULL);
     }
+
+    free(ids);
+    free(customers);
+    free(taxis);
 
     printf("Good night!\n");
 
